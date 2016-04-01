65221c4605df1b550210cf1b21c97bbc373496d0
Vladimir Veljkovic <vyserees@gmail.com>
2016-04-01 12:11:13 +0200

    migrations

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => 'localhost',
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => 'sklav',
+            'username' => 'root',
+            'password' => '123test',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',

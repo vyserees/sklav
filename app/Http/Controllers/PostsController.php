611237a6e7f34e4996c1f37b15a2eee750bf9b18
Vladimir Veljkovic <vyserees@gmail.com>
2016-04-02 11:12:37 +0200

    posts m/c

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class PostsController extends Controller
+{
+    public function index(){
+        
+    }
+    public function single(){
+        
+    }
+    public function create(){
+        
+    }
+    public function edit(){
+
+    }
+    public function update(){
+        
+    }
+}
